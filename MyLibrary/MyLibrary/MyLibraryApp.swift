2617a195cb460577bb652a9b2c37fe876461971d
Kartikay Mehta <kart12mehta@gmail.com>
2024-07-11 12:32:52 +0530

    Initial commit

---
+++ MyLibrary/MyLibrary/MyLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyLibraryApp.swift
+//  MyLibrary
+//
+//  Created by Kartikay Mehta on 11/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MyLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
